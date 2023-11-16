6472ab1d6d3f19814d0444530bb8cfe1a6c4c3a9
Luan Oak <99412966+luancarvalho7@users.noreply.github.com>
2023-11-16 02:00:33 -0300

    Add files via upload

---
+++ src/assets/pages/homepage/bannersSection/bannerSection.jsx
@@ -0,0 +1,38 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './bannerSection.css'
+
+// import required modules
+import { Autoplay } from 'swiper/modules';
+
+export function BannerSection({checkout, communityLink}) {
+    return (
+        <>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                loop={true}
+                autoplay={{
+                    delay: 6000,
+                    disableOnInteraction: false,
+                }}
+                modules={[Autoplay]}
+                className="bannerSwiper"
+            >
+                <SwiperSlide><img src="https://i.postimg.cc/1zRXXtTb/demo-ban01.png" alt="" onClick={() => { }} /></SwiperSlide>
+                <SwiperSlide><img src="https://i.postimg.cc/4dm3v0X5/demo-ban02.png" alt="" style={{ cursor: "pointer" }}
+                    onClick={() => { window.open(communityLink) }} /></SwiperSlide>
+                <SwiperSlide><img src="https://i.postimg.cc/ZR25wLRh/demo-ban-VIP.png" alt="" style={{ cursor: "pointer" }}
+                    onClick={() => { window.open(checkout, "_blank")
+                    }} /></SwiperSlide>
+            </Swiper>
+        </>
+    );
+}
